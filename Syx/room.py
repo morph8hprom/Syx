7b8301b1d8449a2efb131560aebe498aa47ff665
Noved <morph8hprom@gmail.com>
2018-02-18 12:28:32 -0600

    Added build room function

--- Syx/room.py
+++ Syx/room.py
@@ -1,1 +1,37 @@
 #!/usr/bin/env/ python3
+
+import json
+
+class Room():
+    def __init__(
+        self,
+        id,
+        name,
+        desc,
+        exits):
+        self.id = id
+        self.name = name
+        self.desc = desc
+        self.exits = exits
+
+    def _exits(self, dir):
+        """
+        Verifies that provided exit is in room's list of exits
+        """
+        if dir in self.exits:
+            return self.exits[dir]
+        else:
+            return None
+
+
+def build_room(id):
+    room = None
+    #Opens the file
+    with open("data/room/room{}.json".format(str(id)), 'r') as f:
+        #Reads contents of file and stores in variable jsontext
+        jsontext = f.read()
+        #Decodes information from json file to a dictionary
+        d = json.loads(jsontext, strict = False)
+        d['id'] = id
+        room = Room(**d)
+        return room
