a55fa81692a1c46c761c7745b3954e98191d0356
Noved <morph8hprom@gmail.com>
2018-03-14 20:24:49 -0500

    Added character module

---
+++ Syx/character.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import json
+
+class Character():
+    def __init__(self, name = "", desc = ""):
+        self.name = name
+        self.desc = desc
+
+
+
+def build_char(id):
+    char = None
+    #Opens the file
+    with open("data/char/char{}.json".format(str(id)), 'r') as f:
+        #Reads contents of file and stores in variable jsontext
+        jsontext = f.read()
+        #Decodes information from json file to dictionary
+        d = json.loads(jsontext, strict = False)
+        d['id'] = id
+        char = Character(**d)
+        return char
