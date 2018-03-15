2e9fd891743da398fa8b78a23a7b7ea42320438f
Noved <morph8hprom@gmail.com>
2018-03-14 21:47:02 -0500

    Added character attribute tests

--- tests/test_char.py
+++ tests/test_char.py
@@ -4,6 +4,8 @@ import unittest
 import os
 from Syx.character import *
 
+start_id = 1
+num_of_char = 3
 class CharacterAttributesTestCase(unittest.TestCase):
     """
     Verifies all attributes of character instance
@@ -15,22 +17,31 @@ class CharacterAttributesTestCase(unittest.TestCase):
         for testing purposes
         """
         os.chdir("/home/noved/Projects/HardWay/Syx/Syx")
-        start_id = 1
-        num_of_char = 3
+
         self.chars = {}
         for i in range(start_id, num_of_char):
-            self.chars[i] = build_char(i)
+            self.chars[i] = build_char(i, num_of_char)
 
 
     def test_char_has_name(self):
-        start_id = 1
-        num_of_char = 3
+
         for i in range(start_id, num_of_char):
             att = hasattr(self.chars[i], 'name')
             self.assertTrue(att)
     def test_char_name_is_string(self):
-        start_id = 1
-        num_of_char = 3
+
         for i in range(start_id, num_of_char):
             att = self.chars[i].name
+            self.assertIsInstance(att, str)
+
+    def test_char_has_desc(self):
+
+        for i in range(start_id, num_of_char):
+            att = hasattr(self.chars[i], 'desc')
+            self.assertTrue(att)
+
+    def test_char_desc_is_string(self):
+
+        for i in range(start_id, num_of_char):
+            att = self.chars[i].desc
             self.assertIsInstance(att, str)
