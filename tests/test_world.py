11ceaa0d39911c2d11ee6ea10b309b9869f946f7
Noved <morph8hprom@gmail.com>
2018-03-16 12:11:28 -0500

    Added world tests

--- tests/test_world.py
+++ tests/test_world.py
@@ -7,7 +7,7 @@ from Syx.item import *
 from Syx.character import *
 from Syx.map import *
 from Syx.room import *
-id = 1
+start_id = 1
 num_of_rooms = 5
 num_of_items = 3
 num_of_char = 3
@@ -23,6 +23,19 @@ class WorldAttributeTestCase(unittest.TestCase):
         """
         os.chdir("/home/noved/Projects/HardWay/Syx/Syx")
 
-        cls.map = build_map()
+        cls.map = build_map(start_id, num_of_rooms)
+        cls.items = item_list(start_id, num_of_items)
+        cls.chars = char_list(start_id, num_of_char)
+        cls.world = build_world(cls.map, cls.items, cls.chars)
 
-        cls.world = build_world()
+    def test_world_has_map(self):
+        att = hasattr(self.world, 'map')
+        self.assertTrue(att)
+
+    def test_world_has_items(self):
+        att = hasattr(self.world, 'items')
+        self.assertTrue(att)
+
+    def test_world_has_chars(self):
+        att = hasattr(self.world, 'chars')
+        self.assertTrue(att)
