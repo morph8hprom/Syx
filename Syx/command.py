fa2ee68c8f9153727bb41e8335dd1b820b8c9e6a
Noved <morph8hprom@gmail.com>
2018-03-28 19:12:41 -0500

    defined Command class

--- Syx/command.py
+++ Syx/command.py
@@ -1,7 +1,13 @@
 import textwrap
-from world import *
-from map import *
-from room import *
-from item import *
-from character import *
+from Syx.world import *
+from Syx.map import *
+from Syx.room import *
+from Syx.item import *
+from Syx.character import *
 import cmd
+
+class Command(cmd.Cmd):
+    def __init__(self, world):
+        self.prompt = ">"
+        self.world = world
+        self.player_loc = self.world.start_loc
