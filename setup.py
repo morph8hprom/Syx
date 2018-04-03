556720f1ac8cede6338e49c3379a19d7e110c314
Noved <morph8hprom@gmail.com>
2018-04-03 14:00:50 -0500

    Updated build files

--- setup.py
+++ setup.py
@@ -1,7 +1,24 @@
+
+import ez_setup
+ez_setup.use_setuptools()
 from setuptools import setup, find_packages
-from codecs import open
-from os import path
+
+
 
 setup(name = "Syx",
-version = '0.1',
+version = '0.1.0',
+python_requires = '>=3',
+install_requires = ['cmd2>=0.8.2']
+description = 'A text based roleplaying/adventure game',
+url = 'https://www.github.com/morph8hprom/Syx',
+author = 'Devon Blackburn',
+author_email = 'morph8hprom@gmail.com',
+license = 'GPL 3.0'
+packages = find_packages('src'), package_dir = {'' : 'src'}
+include_package_data = True
+package_data = {
+'' : ['*.txt'],
+'src' : ['data/*.json'],
+}
+
 )
